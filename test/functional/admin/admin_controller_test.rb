383c73b6ac317a69e3328b53c5ad8229bb55349c
Andrew Horsman <minirobotics@gmail.com>
2011-06-15 13:28:26 -0400

    Finished initial tests, Code to Test ratio of 1:1.1!

--- test/functional/admin/admin_controller_test.rb
+++ test/functional/admin/admin_controller_test.rb
@@ -1,6 +1,38 @@
 require 'test_helper'
 
 class Admin::AdminControllerTest < ActionController::TestCase
+  def login_as_admin
+    session[:user_id] = 2
+  end
+
+  def login_as_user
+    session[:user_id] = 1
+  end
+
+  test "should display new article link on console" do
+    login_as_admin
+    get :index
+    assert_select "a[href='/admin/articles/new']"
+  end
+
+  test "should display articles table on console" do
+    login_as_admin
+    get :index
+    assert_select "#full-content table"
+    assert_select "#full-content table tbody tr", :count => Article.count
+  end
+
+  test "should redirect to login form if logged out" do
+    get :index
+    assert_redirected_to login_form_path
+  end
+
+  test "should redirect to login form if a regular user" do
+    login_as_user
+    get :index
+    assert_redirected_to login_form_path
+  end
+
   # Routes
   test "should route to administration console" do
     assert_routing "/admin", { :controller => "admin/admin", :action => "index" }
--- test/functional/admin/articles_controller_test.rb
+++ test/functional/admin/articles_controller_test.rb
@@ -1,6 +1,85 @@
 require 'test_helper'
 
 class Admin::ArticlesControllerTest < ActionController::TestCase
+  def setup
+    session[:user_id] = 2
+  end
+
+  test "should commit an article that was last commited over six hours ago" do
+    get :commit, :id => 3
+    assert_redirected_to admin_path
+    assert_equal "Committed!", flash[:notice]
+    assert_equal articles(:four).stage + 1, Article.find(3).stage
+  end
+
+  test "should not commit an article which was just committed" do
+    get :commit, :id => 4
+    assert_redirected_to admin_path
+    assert_equal "Unable to commit at this time.", flash[:notice]
+    assert_equal articles(:four).stage, Article.find(4).stage
+  end
+
+  test "should publish an article if it has reached the final stage" do
+    get :publish, :id => 2
+    assert_redirected_to admin_path
+    assert_equal "Published!", flash[:notice]
+    assert_not_nil Article.find(2).published_at
+  end
+
+  test "should not publish an article which has not reached the final stage" do
+    get :publish, :id => 3
+    assert_redirected_to admin_path
+    assert_equal "Unable to publish at this time.", flash[:notice]
+    assert_nil Article.find(2).published_at
+  end
+
+  test "should unpublish an article" do
+    get :unpublish, :id => 5
+    assert_redirected_to admin_path
+    assert_equal "Unpublished.", flash[:notice]
+    assert_nil Article.find(5).published_at
+  end
+
+  test "should destroy an article" do
+    delete :destroy, :id => 5
+    assert_redirected_to admin_path
+    assert_raise(ActiveRecord::RecordNotFound) do
+      Article.find(5)
+    end
+  end
+
+  test "should update an article" do
+    put :update, :id => 2, :article => { :title => "Testing!" }
+    assert_redirected_to admin_path
+    assert_equal "Testing!", Article.find(2).title
+  end
+
+  test "should not update an article if the title is blank" do
+    put :update, :id => 2, :article => { :title => "" }
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "should not update an article if the author is blank" do
+    put :update, :id => 2, :article => { :author => "" }
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "should not update an article if the content is blank" do
+    put :update, :id => 2, :article => { :content => "" }
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "should update tags" do
+    put :update, :id => 8, :article => { :tag_list => "foo bar" }
+    assert_redirected_to admin_path
+    assert Article.find(8).tags.exists? Tag.find_by_keyword("foo")
+    assert Article.find(8).tags.exists? Tag.find_by_keyword("bar")
+    assert_equal 2, Article.find(8).tags.count
+  end
+
   # Routes
   test "should route to an article commit operation" do
     assert_routing "/admin/articles/1/commit", { :controller => "admin/articles", :action => "commit", :id => "1" }
