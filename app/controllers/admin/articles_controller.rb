ddea5ab7dd4032b6e8ef5a727c66070e7dcd7b94
Andrew Horsman <minirobotics@gmail.com>
2011-06-25 21:53:15 -0400

    Fixed bug when adding a new article (minor spelling).

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -22,7 +22,7 @@ class Admin::ArticlesController < ApplicationController
       @article = Article.new(params[:article])
     end
     redirect_or_render(admin_path, :new) do
-      @articlel.update_html = true
+      @article.update_html = true
       @article.save
     end
   end
