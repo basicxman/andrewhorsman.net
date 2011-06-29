26d0a1d1cd8709fd2f899fd84e00e0f38e9ead14
Andrew Horsman <minirobotics@gmail.com>
2011-06-29 19:32:18 -0400

    Added factories.

---
+++ test/factories.rb
@@ -0,0 +1,33 @@
+Factory.define :article do |f|
+  f.sequence(:title) { |n| "Foo #{n}" }
+  f.sequence(:published_at) { |n| Time.zone.now + n.minutes }
+
+  f.author  "John Smith"
+  f.content "Lipsum"
+  f.stage   3
+  f.last_commit_date Time.zone.now
+  f.update_html true
+end
+
+Factory.define :tag do |f|
+  f.sequence(:keyword) { |n| "foo-#{n}" }
+end
+
+Factory.define :reading_item do |f|
+  f.link "http://andrewhorsman.net"
+  f.name "Andrew Horsman"
+  f.rating 10
+end
+
+Factory.define :admin, :class => User do |f|
+  f.email    "self@andrewhorsman.net"
+  f.name     "Andrew Horsman"
+  f.password "testing"
+  f.is_admin true
+end
+
+Factory.define :user do |f|
+  f.email    "foo@bar.com"
+  f.name     "John Smith"
+  f.password "testing"
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,19 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  fixtures :all
-
   def get_config(sym)
     Site::Application.config.send(sym)
+  end
+
+  def login_as_admin
+    user = Factory(:admin)
+    session[:user_id] = user.id
+    user.id
+  end
+
+  def login_as_user
+    user = Factory(:user)
+    session[:user_id] = user.id
+    user.id
   end
 end
