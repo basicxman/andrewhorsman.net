43717d792511fef5598c316c435c0866a663aa07
Andrew Horsman <minirobotics@gmail.com>
2011-07-04 22:49:20 -0400

    Used proper next/previous page path helpers.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,4 +4,12 @@ module ArticlesHelper
     link_to article.title, article_path(article), :title => article.title
   end
 
+  def np_page_path(page_number, query = nil)
+    if query.nil?
+      articles_page_path(page_number)
+    else
+      search_path(query, :page => page_number)
+    end
+  end
+
 end
