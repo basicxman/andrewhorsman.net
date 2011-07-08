cc10f4c06f9a540f68430f7e0cfcf862c09b10ad
Andrew Horsman <minirobotics@gmail.com>
2011-07-08 17:29:33 -0400

    Drag and drop an article file to fill the form implemented.

--- app/assets/javascripts/admin/articles.js
+++ app/assets/javascripts/admin/articles.js
@@ -1,2 +1,48 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+  if ($(".article-admin-form")) {
+    stopFormBrowserDefault("dragover");
+    stopFormBrowserDefault("dragenter");
+    $(".article-admin-form").get(0).addEventListener("drop", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      var file = e.dataTransfer.files[0];
+      var reader = new FileReader();
+      reader.onprogress = function(e) { };
+      reader.onload = function(e) {
+        fillForm(e.target.result);
+      };
+      reader.readAsText(file);
+    }, false);
+  }
+});
+
+function stopFormBrowserDefault(event) {
+  $(".article-admin-form").bind(event, function(e) { e.preventDefault(); });
+}
+
+function fillForm(data) {
+  var content = data.split("\n");
+  var header  = new Array();
+
+  var i = 0;
+  for (; i < content.length; ++i) {
+    if (content[i].trim() == "") break;
+    header.push(content.shift());
+    --i;
+  }
+
+  if (i == content.length - 1) {
+    content = header.join("\n");
+  } else {
+    $("#article_content").val(content.join("\n").trim());
+
+    for (var i = 0; i < header.length; ++i) {
+      attrs = header[i].match(/@([a-z]+):\s*(.*)/);
+      if (attrs[1] == "tags")
+        $("#article_tag_list").val(attrs[2]);
+      else
+        $("#article_" + attrs[1]).val(attrs[2]);
+    }
+  }
+}
