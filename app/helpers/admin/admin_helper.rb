1ef0e74cf786eabd4b4249951f3d5244b6862db5
Andrew Horsman <minirobotics@gmail.com>
2011-07-08 23:38:42 -0400

    Er, minor fix.

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -9,7 +9,9 @@ module Admin::AdminHelper
 
   # Calculate amount of time until a commit can be made.
   def time_required(time)
+    return "Go" if time.nil?
     target_time = time + get_config(:required_commit_time_delta).hours
+
     time_ago_in_words(target_time)
   end
 
